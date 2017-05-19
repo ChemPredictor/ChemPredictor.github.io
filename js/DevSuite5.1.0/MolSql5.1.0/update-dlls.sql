8cf712e85cf1ef0be272bd63e7fa822a7fd63947
Ameet Rahane <ameetrahane@gmail.com>
2017-05-19 14:29:23 -0600

    added library for drawing

--- js/DevSuite5.1.0/MolSql5.1.0/update-dlls.sql
+++
@@ -1,5 +0,0 @@
-use [TARGETDATABASE];
-alter ASSEMBLY [Scilligence.MolEngine] from 'PATH\Scilligence.MolEngine.dll' with unchecked data;
-alter ASSEMBLY [Scilligence.MolSql] from 'PATH\Scilligence.MolSql.dll' with unchecked data;
-GO
-
